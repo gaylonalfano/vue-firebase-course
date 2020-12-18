d6b124ba1d38ae1fd63d10d084623f64fbb2e424
gaylonalfano <gaylon.alfano@gmail.com>
2020-12-18 08:28:35 +0800

    Testing new GH access after entering creds.

--- dojo-blog/src/views/Home.vue
+++ dojo-blog/src/views/Home.vue
@@ -17,42 +17,6 @@ export default defineComponent({
   name: "Home",
   components: { PostList },
   setup() {
-    interface Post {
-      id: number;
-      title: string;
-      body: string;
-      tags: string[];
-    }
-
-    const posts = ref<Post[]>([]);
-    // Create error variable in case of error with HTTP request
-    // We can return this to make it availabe in the template to display
-    const error = ref<string | null>(null);
-
-    // Now let's fetch the data from json-server
-    const load = async () => {
-      try {
-        // Use await so it won't run the next line below. It waits.
-        const data = await fetch("http://localhost:3000/posts");
-        // console.log(data); // Response
-        // Check that Response is NOT okay via 'ok' property
-        if (!data.ok) {
-          // Response isn't ok so throw an error
-          // NOTE This new Error() Object will be passed into catch(err)
-          throw Error("No data available at API."); // Stored in Error.message
-        }
-
-        // Let's parse the Response JSON into JS and store in posts.value
-        posts.value = await data.json();
-      } catch (err) {
-        // We can do something now with the caught Error
-        // We can even update our error ref()'s value to Error.message
-        error.value = err.message;
-        // Confirm that error is reactive and we've updated its value
-        console.log(error.value);
-      }
-    };
-
     // Run our load() to fetch data
     load();
 
