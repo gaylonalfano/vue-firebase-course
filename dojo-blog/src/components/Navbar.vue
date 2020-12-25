9f12a9fefe2bcd83d34b04e160a1433b4cfd6fe4
gaylonalfano <gaylon.alfano@gmail.com>
2020-12-25 12:14:52 +0800

    Added a Navbar component with some basic styling. Learned that justify-content: space-between will separate the elements within your header element, in this case are my h1 and the nav elements. This separates them on left and right sides. Next, diving into using the router within setup() method.

---
+++ dojo-blog/src/components/Navbar.vue
@@ -0,0 +1,40 @@
+<template>
+  <header>
+    <h1>The Dojo Blog</h1>
+    <nav>
+      <router-link :to="{ name: 'Home' }">Home</router-link>
+      <router-link :to="{ name: 'CreatePost' }">Create Post</router-link>
+    </nav>
+  </header>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({
+  name: "Navbar",
+});
+</script>
+
+<style>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  max-width: 1200px;
+  margin: 0 auto;
+  padding: 10px;
+}
+header h1 {
+  color: #dfdfdf;
+  font-size: 48px;
+}
+header a {
+  color: #bbb;
+  text-decoration: none;
+  margin-left: 20px;
+}
+header a.router-link-active {
+  color: #444;
+  font-weight: bold;
+}
+</style>
