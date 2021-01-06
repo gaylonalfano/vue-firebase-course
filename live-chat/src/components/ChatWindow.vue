d74e5559cb18010fb0e99b3d79305ad028c3ecc7
gaylonalfano <gaylon.alfano@gmail.com>
2021-01-06 21:01:50 +0800

    Discovered I could change the createdAt data format by using the formatDistanceToNow() directly inside the template. Must easier and I don't believe there is any real performance differene between the two. Also added the ability to auto-scroll to the bottom of the messages div. Had to use a Template Ref, which is still a Vue ref() but it can be added as an attribute inside an HTML element that then gives you direct access to that Element via the refName.value. With this, I learned how to auto-scroll to the bottom after a new message has been added. This triggers a rerender of the component, so we used the onUpdated() life-cycle method to perform the auto-scroll on our Template Ref using El.scrollTop = El.scrollHeight. Neat stuff.

--- live-chat/src/components/ChatWindow.vue
+++ live-chat/src/components/ChatWindow.vue
@@ -3,13 +3,11 @@
     <div v-if="error">
       {{ error }}
     </div>
-    <div v-if="formattedMessages" class="messages">
-      <div
-        v-for="message in formattedMessages"
-        :key="message.id"
-        class="single-message"
-      >
-        <span class="created-at">{{ message.createdAt }}</span>
+    <div v-if="messages" class="messages" ref="messagesDivRef">
+      <div v-for="message in messages" :key="message.id" class="single-message">
+        <span class="created-at">{{
+          formatDistanceToNow(message.createdAt.toDate())
+        }}</span>
         <span class="name">{{ message.name }}</span>
         <span class="message">{{ message.message }}</span>
       </div>
@@ -18,14 +16,9 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, computed } from "vue";
+import { defineComponent, computed, ref, onUpdated } from "vue";
 import { formatDistanceToNow } from "date-fns";
 import getCollection from "@/composables/getCollection";
-import {
-  QuerySnapshot,
-  QueryDocumentSnapshot,
-  DocumentData,
-} from "@firebase/firestore-types";
 
 export default defineComponent({
   name: "ChatWindow",
@@ -34,27 +27,56 @@ export default defineComponent({
     // Q: Do I need to await or something? Otherwise documents.value is null initially
     // Can rename variables while destructuring! Neat!
     const { error, documents: messages } = getCollection("messages");
-    console.log("ChatWindow:messages.value: ", messages.value); // null
+    // console.log("ChatWindow:messages.value: ", messages.value); // null
 
+    // Create a Template Ref for our div.messages element
+    // NOTE This Ref is initially null
+    const messagesDivRef = ref<HTMLDivElement | null>(null);
+    // console.log(messagesDivRef, messagesDivRef.value); // RefImpl, null
+
+    // Use onUpdated() to grab messagesDivRef element and make it auto-scroll down
+    onUpdated(() => {
+      console.log("onUpdated Triggered!");
+      // console.log(messagesDivRef, messagesDivRef.value); // RefImpl, <div class="messages">
+
+      // Let's access our messages HTMLDivElement and make it auto-scroll to bottom
+      if (messagesDivRef.value) {
+        // === Using Element.scrollTop = Element.scrollHeight (older approach)
+        messagesDivRef.value.scrollTop = messagesDivRef.value?.scrollHeight; // WORKS!
+        // === Using Element.scrollIntoView() seems like a modern solution
+        // messagesDivRef.value.scrollIntoView({
+        //   behavior: "smooth",
+        //   block: "end",
+        //   inline: "nearest",
+        // }); // NOPE!
+        // messagesDivRef.value.scrollIntoView(false); // NOPE!
+      }
+    });
+
+    // ==== Format Date Method 1: Use computed() to create new array with new time
     // Create a new computed property. We used computed() to do this
     // because it's based on something that ALREADY exists!
     // In this case, we're going to compute formattedMessages array
     // That updates/formats the createdAt property.
-    const formattedMessages = computed(() => {
-      // Check that we actually have documents
-      if (messages.value) {
-        return messages.value.map((message) => {
-          // ERROR: I get TS error if I don't use message.data()
-          // It compiles if I use TS IGNORE but not ideal
-          // @ts-ignore
-          let time = formatDistanceToNow(message.createdAt.toDate());
-          // Return a new object with updated createdAt value
-          return { ...message, createdAt: time };
-        });
-      }
-    });
+    // const formattedMessages = computed(() => {
+    //   // Check that we actually have documents
+    //   if (messages.value) {
+    //     return messages.value.map((message) => {
+    //       // ERROR: I get TS error if I don't use message.data()
+    //       // It compiles if I use TS IGNORE but not ideal
+    //       // @ts-ignore
+    //       let time = formatDistanceToNow(message.createdAt.toDate());
+    //       // Return a new object with updated createdAt value
+    //       return { ...message, createdAt: time };
+    //     });
+    //   }
+    // });
 
-    return { error, messages, formattedMessages };
+    // ==== Format Date Method 2: Use the formatDistanceToNow() directly in template
+    // Q: Can I just do this conversion directly inside the template?
+    // A: Yes! Can call it using {{ formatDistanceToNow(message.createdAt.toDate()) }}
+
+    return { error, messages, formatDistanceToNow, messagesDivRef };
   },
 });
 </script>
