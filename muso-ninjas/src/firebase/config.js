e5a880a76796b32a9375e985c1050a04b141584c
gaylonalfano <gaylon.alfano@gmail.com>
2021-01-11 15:05:03 +0800

    Worked on CreatePlaylist.vue component and learned about input type=file for file uploads. Added a change envent listener and we can access file properties from the input element using El.files which is a FileList type. Also added the Firebase Storage so wecan start handling saving images. Read notes and comments.

--- muso-ninjas/src/firebase/config.js
+++ muso-ninjas/src/firebase/config.js
@@ -1,6 +1,7 @@
 import firebase from "firebase/app";
 import "firebase/firestore";
 import "firebase/auth";
+import "firebase/storage";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBpdYL8KJlvU6Z3KIcHijjP660tapwN3f4",
@@ -11,9 +12,10 @@ const firebaseConfig = {
   appId: "1:1018236482277:web:41774e8772d78998fa9a0c",
 };
 
-// Init Firebase App connection to backend
+// === Init Firebase App connection to backend
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 
+// === Init Services
 // Initialize Authentication Service
 const auth = firebase.auth(); // Or, projectAuth
 
@@ -22,11 +24,13 @@ const auth = firebase.auth(); // Or, projectAuth
 // to add documents, retrieve collections, etc.
 const db = firebase.firestore(firebaseApp); // Or, projectFirestore
 
+// Initialize Firebase Storage Service
+const storage = firebase.storage();
+
+// === Timestamp
 // Let's add a timestamp FieldValue to export to other files.
 // NOTE We are storing a FUNCTION REFERENCE in timestamp
 const timestamp = firebase.firestore.FieldValue.serverTimestamp;
 
-// NOTE Later we'll add the Authentication Service
-
 // Export it so we can import/use Firebase into other files/components
-export { auth, db, timestamp };
+export { auth, db, storage, timestamp };
--- muso-ninjas/src/views/playlists/CreatePlaylist.vue
+++ muso-ninjas/src/views/playlists/CreatePlaylist.vue
@@ -9,8 +9,9 @@
     ></textarea>
     <!-- upload playlist image -->
     <label>Upload playlist cover image</label>
-    <input type="file" />
-    <div v-if="error" class="error">{{ error }}</div>
+    <input type="file" @change="handleFileChange" />
+    <!-- <div v-if="error" class="error">{{ error }}</div> -->
+    <div v-if="fileError" class="error">{{ fileError }}</div>
     <button v-if="!isPending">Create</button>
     <button v-if="isPending" disabled>Loading...</button>
   </form>
@@ -29,38 +30,149 @@ export default defineComponent({
     // Data properties Refs
     const title = ref<string>("");
     const description = ref<string>("");
+    // Handle file upload changes. Use 'File' TS type
+    const coverImageFile = ref<File | null>(null);
+    const fileError = ref<string | null>(null);
 
     // Handle form submission to add document (playlist)
     // NOTE addDoc is async so need to make this async as well so we can await
     async function handleCreatePlaylist() {
-      // Package up user inputs into a playlist object
-      const playlist = { title: title.value, description: description.value };
+      // Check that a file (and it's valid) has been uploaded
+      if (coverImageFile.value) {
+        // console.log(title.value, description.value, coverImageFile.value);
+        // Package up user inputs into a playlist object
+        const playlist = { title: title.value, description: description.value };
 
-      // Use addDoc(playlist) to try to create the document
-      // NOTE addDoc is async so need to await
-      await addDoc(playlist);
+        // Use addDoc(playlist) to try to create the document
+        // NOTE addDoc is async so need to await
+        await addDoc(playlist);
 
-      if (!error.value) {
-        console.log("SUCCESS:handleCreatePlaylist:playlist: ", playlist);
-      } // Else, display error inside template
+        if (!error.value) {
+          console.log("SUCCESS:handleCreatePlaylist:playlist: ");
+          // NOTE No need to reset error.value since already null
+        } // Else, display error inside template
+      } else {
+        // Inform user that a valid file must be uploaded
+        fileError.value = "Please select an image file (png or jpeg)";
+      }
+    }
+
+    // Allowed file types
+    const types: string[] = ["image/png", "image/jpeg"];
+
+    function handleFileChange(e: Event) {
+      // Q: Create Template Ref inside this function?
+      // A: NO. Outside like most other refs.
+      // Need to watch/track this input field's value and update when changed
+      console.log("handleFileChange:event.target:", e.target); // <input type="file">
+      // console.log(e.target); // <input type="file">
+      const inputFiles: FileList | null = (e.target as HTMLInputElement).files; // Works
+      // const inputFile: File = inputFiles[0]; // Error possibly null
+      console.log("BEFOREConditionals::inputFiles: ", inputFiles); // FileList
+      console.log(
+        "BEFOREConditionals::inputFiles?length: ",
+        inputFiles?.length
+      ); // 1, 0
+
+      // === Method 1: Nested if statements. WORKS but ugly.
+      // Need to handle if no file is uploaded at all.
+      // if (inputFiles && inputFiles.length) {
+      //   console.log("PASSED:inputFiles && inputFiles.length");
+      //   // We have a file to work with. Update coverImageFile Ref value
+      //   const inputFile: File = inputFiles[0]; // .name, .size, .type
+      //   // Check that file types is compatible before blindly saving to our Ref
+      //   if (inputFile && types.includes(inputFile.type)) {
+      //     console.log("PASSED:inputFile && types.includes(inputFile.type)");
+      //     // Finally feel good to update ref value with inputFile
+      //     coverImageFile.value = inputFile;
+      //     console.log(
+      //       "UPDATED:handleFileChange:coverImageFile.value: ",
+      //       coverImageFile.value
+      //     );
+      //     // Reset any possible fileError ref back to null
+      //     fileError.value = null;
+      //   } else {
+      //     // Reset Ref value to null just in case
+      //     coverImageFile.value = null;
+      //     // Update fileError ref
+      //     fileError.value = "Please select an image file (png or jpeg)";
+      //   }
+      // } else {
+      //   // Reset Ref value to null just in case
+      //   coverImageFile.value = null;
+      //   // Update fileError ref
+      //   fileError.value = "Please select an image file (png or jpeg)";
+      // }
+
+      // === Method 2: Multiple && conditions. WORKS and more DRY.
+      // TODO Refactor. Had to break it up due to TS errors w/ possible 'null' values
+      // Check that we have a file or not and update Ref
+      if (
+        inputFiles?.length &&
+        inputFiles[0] &&
+        types.includes(inputFiles[0].type)
+      ) {
+        console.log("PASSED:inputFiles && inputFiles.length");
+        console.log("PASSED:types.includes(inputFiles[0].type)");
+        // We have a file to work with. Update coverImageFile Ref value
+        const inputFile: File = inputFiles[0]; // .name, .size, .type
+        // Check that file types is compatible before blindly saving to our Ref
+        // if (types.includes(inputFile.type)) {
+        // Finally feel good to update ref value with inputFile
+        coverImageFile.value = inputFile;
+        console.log(
+          "UPDATED:handleFileChange:coverImageFile.value: ",
+          coverImageFile.value
+        );
+        // Reset any possible fileError ref back to null
+        fileError.value = null;
+      } else {
+        // Reset Ref value to null just in case
+        coverImageFile.value = null;
+        // Update fileError ref
+        fileError.value = "Please select an image file (png or jpeg)";
+      }
+
+      // === INITIAL TESTING ======
+      // console.log(coverImage.value); // null
+      // const coverImageFileComputed = computed(() => coverImage.value);
+      // console.log("coverImageFileComputed: ", coverImageFileComputed); // ComputedRefImpl value: null
+
+      // Q: Do I use a Template Ref to access the <input>?
+      // A: NO! We use a Ref, but not a Template Ref. We get the
+      // HTMLInputElement via event.target
+      // Q: How to access 'this' or 'event.target' without a Ref?
+      // A: Pass the 'event' arg to the handleFileChange(event)!
+      // Update ref value on change
+      // coverImage.value = coverImage.value?.value; // ERROR
+      // Q: How to ensure coverImage.value has a value?
+      // A: onUpdated() but we're not reloading the component
+      // A: We just need to check for e.target.files[0]
+
+      // === BROKEN: Tried Template Refs ===
+      // Get the uploaded file properties using .files[0].name
+      // if (coverImageFile.value) {
+      //   console.log(coverImageFile.value); // ?
+      // }
+      // console.log(coverImageFile.value?.files); // undefined
+      // console.log(coverImageFile.value); // null
     }
 
     return {
       title,
       description,
       error,
+      fileError,
       addDoc,
       isPending,
       handleCreatePlaylist,
+      handleFileChange,
     };
   },
 });
 </script>
 
 <style>
-form {
-  background: white;
-}
 input[type="file"] {
   border: 0;
   padding: 0;
