2a96575e32e4ad6aa8ff68acce6e10cfee6b1163
gaylonalfano <gaylon.alfano@gmail.com>
2021-02-09 20:45:46 +0800

    Learned about Background Triggers and HTML Triggers. We started with a Endpoint Request HTTP Trigger. The key thing is that we use functions.https to create these HTTP Trigger functions. Any function we make and deploy will be given an endpoint URL. We literally make HTTP GET requests to these endpoints, which will invoke these functions to do their thing. We're going to learn about the other type of HTTP Trigger, which is the type that we can invoke directly, unlike Background Triggers types.

--- firebase-cloud-functions/functions/src/index.ts
+++ firebase-cloud-functions/functions/src/index.ts
@@ -2,8 +2,23 @@ import * as functions from "firebase-functions";
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
-//
 // export const helloWorld = functions.https.onRequest((request, response) => {
 //   functions.logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+
+// HTTP request 1
+export const randomNumber = functions.https.onRequest((request, response) => {
+  // The HTTP handler will run when request is made to the randomNumber function
+  const number = Math.round(Math.random() * 100);
+  // console.log(number); // Gets added to FB Functions > Logs tab.
+  response.send(number.toString());
+});
+
+// HTTP request 2
+export const redirectToMySite = functions.https.onRequest(
+    (request, response) => {
+    // Use the response.redirect()
+      response.redirect("https://www.gaylonalfano.com");
+    }
+);
